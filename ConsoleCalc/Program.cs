9a43a177290485eb1f52ce21520c3b40c0086347
ц <>
2018-02-05 20:47:35 +0400

    1. Добавлены в калькулятор еще 4 функции 2. Добавлены к ним тесты 3. Выбор операции в консоли, с сохранением работы через параметры 4.* Переход с int/string на double

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using ConsoleCalc;
 
 namespace ConsoleCalc
 {
@@ -13,19 +14,46 @@ namespace ConsoleCalc
             Console.WriteLine("Калькулятор");
 
             var oper = args[0];
+            var x = double.Parse(args[1]);
+            var y = double.Parse(args[2]);
+
+            Сalculation(oper, x, y);
+            Console.ReadKey();
+        }
+
+        static void Сalculation(string oper, double x, double y)
+        {
+            var calc1 = new Calc();
             if (oper == "sum")
             {
-                var x = Convert.ToInt32(args[1]);
-                var y = Convert.ToInt32(args[2]);
-                var result = x + y;
-                
-                Console.WriteLine($"SUM({x}, {y}) = {result}");
+                Console.WriteLine($"SUM({x}, {y}) = {calc1.Sum(x, y)}");
+            }
+            else if (oper == "sub")
+            {
+                Console.WriteLine($"SUB({x}, {y}) = {calc1.Sub(x, y)}");
+            }
+            else if (oper == "mult")
+            {
+                Console.WriteLine($"MULT({x}, {y}) = {calc1.Mult(x, y)}");
+            }
+            else if (oper == "segm")
+            {
+                Console.WriteLine($"SEGM({x}, {y}) = {calc1.Segm(x, y)}");
+            }
+            else if (oper == "pow")
+            {
+                Console.WriteLine($"POW({x}, {y}) = {calc1.Pow(x, y)}");
             }
             else
             {
-                Console.WriteLine("null");
+                Console.WriteLine("Введите операцию");
+                oper = Console.ReadLine();
+                Console.WriteLine("Введите первый аргумент");
+                x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второй аргумент");
+                y = double.Parse(Console.ReadLine());
+                Сalculation(oper, x, y);
             }
-            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleTest/CalcTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCalc;
+
+namespace ConsoleTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+
+        [TestMethod]
+        public void TestSub()
+        {
+            //Arrange
+            var calc = new Calc();
+            var x = 10;
+            var y = 5.5;
+
+            //Act
+            var result = calc.Sub(x, y);
+            var result3 = calc.Segm(x, y);
+            var result4 = calc.Pow(x, y);
+
+            //Assert
+            Assert.AreEqual(4.5, result);
+        }
+
+        [TestMethod]
+        public void TestSum()
+        {
+            //Arrange
+            var calc = new Calc();
+            var x = 10;
+            var y = 5;
+
+            //Act
+            var result = calc.Sum(x, y);
+
+            //Assert
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void TestMult()
+        {
+            //Arrange
+            var calc = new Calc();
+            var x = 10;
+            var y = 5;
+
+            //Act
+            var result = calc.Mult(x, y);
+
+            //Assert
+            Assert.AreEqual(50, result);
+        }
+
+        [TestMethod]
+        public void TestSegm()
+        {
+            //Arrange
+            var calc = new Calc();
+            var x = 10;
+            var y = 5;
+
+            //Act
+            var result = calc.Segm(x, y);
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestPow()
+        {
+            //Arrange
+            var calc = new Calc();
+            var x = 10;
+            var y = 5;
+
+            //Act
+            var result = calc.Pow(x, y);
+
+            //Assert
+            Assert.AreEqual(100000, result);
+        }
+    }
+}
