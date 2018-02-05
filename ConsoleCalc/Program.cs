2a605e410fe79990141660124dc5e176399d0cb5
ц <>
2018-02-05 19:20:52 +0400

    автотест

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -11,6 +11,20 @@ namespace ConsoleCalc
         static void Main(string[] args)
         {
             Console.WriteLine("Калькулятор");
+
+            var oper = args[0];
+            if (oper == "sum")
+            {
+                var x = Convert.ToInt32(args[1]);
+                var y = Convert.ToInt32(args[2]);
+                var result = x + y;
+                
+                Console.WriteLine($"SUM({x}, {y}) = {result}");
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
             Console.ReadKey();
         }
     }
