b777afa26109021fd4db64a231a0a3a0671946a3
Кристина <chauzova@elewise.com>
2018-02-06 20:57:46 +0400

    home work 2

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,50 +11,37 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
+            var calc = new Calc();
+            var operations = calc.GetOperNames();
             Console.WriteLine("Калькулятор");
 
-            var oper = args[0];
-            var x = double.Parse(args[1]);
-            var y = double.Parse(args[2]);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Введите операцию");
+                foreach (var item in operations)
+                {
+                    Console.WriteLine(item);
+                }
+                string oper = Console.ReadLine();
 
-            Сalculation(oper, x, y);
+                Console.WriteLine("Введите параметры через запятую");
+                string x = Console.ReadLine();
+                args = new[] { oper, x };
+            }
+
+            Сalculation(args[0], args[1]);
+
             Console.ReadKey();
         }
 
-        static void Сalculation(string oper, double x, double y)
+        static void Сalculation(string oper, string x)
         {
-            //нужно дкелать через switch case
             var calc = new Calc();
-            if (oper == "sum")
-            {
-                Console.WriteLine($"SUM({x}, {y}) = {calc.Sum(x, y)}");
-            }
-            else if (oper == "sub")
-            {
-                Console.WriteLine($"SUB({x}, {y}) = {calc.Sub(x, y)}");
-            }
-            else if (oper == "mult")
-            {
-                Console.WriteLine($"MULT({x}, {y}) = {calc.Mult(x, y)}");
-            }
-            else if (oper == "segm")
-            {
-                Console.WriteLine($"SEGM({x}, {y}) = {calc.Segm(x, y)}");
-            }
-            else if (oper == "pow")
-            {
-                Console.WriteLine($"POW({x}, {y}) = {calc.Pow(x, y)}");
-            }
-            else
-            {
-                Console.WriteLine("Введите операцию");
-                oper = Console.ReadLine();
-                Console.WriteLine("Введите первый аргумент");
-                x = double.Parse(Console.ReadLine());
-                Console.WriteLine("Введите второй аргумент");
-                y = double.Parse(Console.ReadLine());
-                Сalculation(oper, x, y);
-            }
+            double[] a = x.Split(',').Select(s => double.Parse(s)).ToArray();
+
+            var result = calc.Exec(oper, a);
+
+            Console.WriteLine($"{oper}({String.Join(",", a)}) = {result}");
         }
     }
 }
---
+++ ITUniver.Calc.Core/Calc.cs
@@ -0,0 +1,69 @@
+﻿using ITUniver.Calc.Core.Interface;
+using ITUniver.Calc.Core.Operations;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleCalc
+{
+    public class Calc
+    {
+        private IList<IOperation> operations { get; set; }
+
+        public Calc()
+        {
+            operations = new List<IOperation>();
+            var assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes();
+            foreach (var item in types)
+            {
+                var interfaces = item.GetInterfaces();
+                var isOperation = interfaces.Any(
+                    it => it == typeof(IOperation));
+
+                if (isOperation)
+                {
+                    //создаем экз. объекта
+                    var obj = Activator.CreateInstance(item);
+                    //приводим его в операцию
+                    var operation = obj as IOperation;
+                    //если удалось
+                    if (operation != null)
+                    {
+                        //добавляем в список операций
+                        operations.Add(operation);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получить список имен операций
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetOperNames()
+        {
+            return operations.Select(o => o.Name).ToArray();
+        }
+        public double Exec(string oper, double[] args)
+        {
+            //найти операцию в списке 
+            var operation = operations
+                .FirstOrDefault(o => o.Name == oper);
+
+            //если не найдена - возвращаем ошибку
+            if (operation == null)
+                return double.NaN;
+
+            //если нашли
+            //передаем ей аргументы и вычисляем результат
+            var result = operation.Exec(args);
+
+            //возвращаем результат 
+            return result;
+        }
+    }
+}
